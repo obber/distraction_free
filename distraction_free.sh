9ba9d86f42fd56b5701455ae379d94e07c43975b
Kan Adachi <Kadachi1023@gmail.com>
2016-01-29 20:34:11 -0800

    Changed overwriting mechanism to sed for safety

--- distraction_free.sh
+++ distraction_free.sh
@@ -8,64 +8,57 @@ youtube.com"
 # The forced destination address
 DESTINATION="127.0.0.1"
 
+# IPv6 forced destination for ISPs that provide IPV6 support
+IPV6_DESTINATION="fe80::1%lo0"
+
 # =================================================================
 # 
 # No need to edit anything below here
 #
 #
 
-# counter for line removal
-COUNT=$(echo "$SITES_TO_DISABLE" | wc -l);
-((COUNT *= 4))
-
 # =======================
-# for "on"
+# for "on" argument
 #
 if [ "$@" == "on" ]; then
   # Error out if DF mode is already on
-  if [[ $(tail -n 1 /etc/hosts) == "# DF ON" ]]; then
+  if [[ $(tail -n 1 /etc/hosts) == "#DF_END" ]]; then
     echo "\033[1;31mDistraction free mode is already on.\033[0m"
     exit;
   fi
 
-  # IPv6 entry for ISPs that provide IPV6 support
-  IPV6="fe80::1%lo0"
-
-  # add white space for clarity
-  echo "\n\n\n" >> /etc/hosts
+  # add white space + #DF_BEGIN
+  echo "\n\n#DF_BEGIN" >> /etc/hosts
 
   # add disable entries
   while read -r LINE; do
     if [[ -n $LINE ]]; then
       echo "$DESTINATION $LINE" >> /etc/hosts
       echo "$DESTINATION www.$LINE" >> /etc/hosts
-      echo "$IPV6 $LINE" >> /etc/hosts
-      echo "$IPV6 www.$LINE" >> /etc/hosts
+      echo "$IPV6_DESTINATION $LINE" >> /etc/hosts
+      echo "$IPV6_DESTINATION www.$LINE" >> /etc/hosts
     fi
   done <<< "$SITES_TO_DISABLE"
 
-  # add "DF ON" line to keep track of on/off status
-  echo "# DF ON" >> /etc/hosts
+  # add "#DF_END" line
+  echo "#DF_END" >> /etc/hosts
 
   # output message
   echo "Distraction Free Mode: \033[1;32mEnabled\033[0m"
   echo "You may need to restart your browser."
 
 # =======================
-# for "off"
+# for "off" argument
 #
 elif [ "$@" == "off" ]; then
   # Error out if DF mode is already on
-  if [[ $(tail -n 1 /etc/hosts) != "# DF ON" ]]; then
+  if [[ $(tail -n 1 /etc/hosts) != "#DF_END" ]]; then
     echo "\033[1;31mLooks like distraction free mode is not on. You might want to manually check /etc/hosts or turn it on first.\033[0m"
     exit;
   fi
 
   # overwrite /etc/hosts, removing $COUNT number of lines.
-  HEADCOUNT=$(wc -l < /etc/hosts)
-  HEADCOUNT=$((HEADCOUNT - COUNT))
-  NEW_HOSTS_FILE="$(head -n $HEADCOUNT /etc/hosts)"
-  echo "$NEW_HOSTS_FILE" > /etc/hosts
+  echo "$(sed '/\#DF_BEGIN/,/\#DF_END/d' /etc/hosts)" > /etc/hosts
 
   # output message
   echo "Distraction Free Mode: \033[1;31mDisabled\033[0m"
